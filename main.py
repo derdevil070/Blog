a8202fd431521ce47d7bb542d0547b3e16944bb9
priyansh hirpara <priyanshhirpara2323@gmail.com>
2024-05-22 14:45:46 +0530

    some

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI, Depends
-from schemas import Blog
+from fastapi import FastAPI, Depends, status, Response, HTTPException
+from schemas import Blog, BlogUpdate
 import models
 from database import engine, sessionLocal
 from sqlalchemy.orm import Session
@@ -17,10 +17,54 @@ def get_db():
         db.close()
 
 
-@app.post("/blog")
+@app.post("/blog", status_code=status.HTTP_201_CREATED)
 def create(request: Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title, body=request.body)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
     return new_blog
+
+
+@app.delete("/blog/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def destory(id, db: Session = Depends(get_db)):
+    blog = (
+        db.query(models.Blog)
+        .filter(models.Blog.id == id)
+        .delete(synchronize_session=False)
+    )
+    if not blog:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Blog with the id {id} is not available",
+        )
+    db.commit()
+    return "Done"
+
+
+@app.put("/blog/{id}", status_code=status.HTTP_202_ACCEPTED)
+def update(id, request: BlogUpdate, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    for key, value in request.model_dump().items():
+        setattr(blog, key, value)
+    db.commit()
+    return "update"
+
+
+@app.get("/blog")
+def all(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+
+@app.get("/blog/{id}", status_code=200)
+def show(id, response: Response, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Blog with the id {id} is not available",
+        )
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {"detail": f"Blog with the id {id} is not avalilable"}
+    return blog
--- schemas.py
+++ schemas.py
@@ -1,6 +1,12 @@
 from pydantic import BaseModel
+from typing import Optional
 
 
 class Blog(BaseModel):
     title: str
     body: str
+
+
+class BlogUpdate(Blog):
+    title: Optional[str] = None
+    body: Optional[str] = None
