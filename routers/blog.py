0a67a44d421c818d17bf4eca4f037b77bc7a0d29
priyansh hirpara <priyanshhirpara2323@gmail.com>
2024-05-29 17:00:31 +0530

    ferw

--- routers/blog.py
+++ routers/blog.py
@@ -10,27 +10,43 @@ Router = APIRouter(prefix="/blog", tags=["Blogs"])
 
 
 @Router.post("/", status_code=status.HTTP_201_CREATED)
-def create(request: Blog, db: Session = Depends(get_db)):
+def create(
+    request: Blog,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user),
+):
     return blog.create(request, db)
 
 
 @Router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def destory(id: int, db: Session = Depends(get_db)):
+def destory(
+    id: int,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user),
+):
     return blog.delete(id, db)
 
 
 @Router.put("/{id}", status_code=status.HTTP_202_ACCEPTED)
-def update(id: int, request: BlogUpdate, db: Session = Depends(get_db)):
+def update(
+    id: int,
+    request: BlogUpdate,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user),
+):
     return blog.update(id, request, db)
 
 
 @Router.get("/", response_model=List[ShowBlog])
-def all(
-    db: Session = Depends(get_db), get_current_user: User = Depends(get_current_user)
-):
+def all(db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
     return blog.get_all(db)
 
 
 @Router.get("/{id}", status_code=200, response_model=ShowBlog)
-def show(id: int, response: Response, db: Session = Depends(get_db)):
+def show(
+    id: int,
+    response: Response,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user),
+):
     return blog.get(id, db, response)
